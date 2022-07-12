c3587d7042e75670870290cebef7cffaa50cfdd7
maina lawrence <mainalawrence32@gmail.com>
2022-07-12 23:38:13 +0300

    app

---
+++ src/app/admin-side/Component/main-admin/main-admin.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+interface SideNavToggle {
+  screenWidth: number;
+  collapsed: boolean;
+}
+
+
+@Component({
+  selector: 'app-main-admin',
+  templateUrl: './main-admin.component.html',
+  styleUrls: ['./main-admin.component.css']
+})
+export class MainAdminComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  isSideNavCollapsed = false;
+  screenWidth = 0;
+
+  onToggleSideNav(data: SideNavToggle): void {
+    this.screenWidth = data.screenWidth;
+    this.isSideNavCollapsed = data.collapsed;
+  }
+
+}
--- src/app/admin-side/admin-side.module.ts
+++ src/app/admin-side/admin-side.module.ts
@@ -1,12 +1,47 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from './Component/products/products.component';
+import { StatisticsComponent } from './Component/statistics/statistics.component';
+import { PagesComponent } from './Component/pages/pages.component';
+import { SettingsComponent } from './Component/settings/settings.component';
+import { DashboardComponent } from './Component/dashboard/dashboard.component';
+import { CoupensComponent } from './Component/coupens/coupens.component';
+import { MediaComponent } from './Component/media/media.component';
+import { MainAdminComponent } from './Component/main-admin/main-admin.component';
+import { BodyComponent } from './Component/body/body.component';
+import { SidenavComponent } from './Component/sidenav/sidenav.component';
+//{path: '', component:MainAdminComponent},
+const routes: Routes = [
+  {path:'',component:MainAdminComponent,children:[
+  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'products', component: ProductsComponent},
+  {path: 'statistics', component: StatisticsComponent},
+  {path: 'coupens', component: CoupensComponent},
+  {path: 'pages', component: PagesComponent},
+  {path: 'media', component: MediaComponent},
+  {path: 'settings', component: SettingsComponent}
+  ]}
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MainAdminComponent,
+    BodyComponent,
+    SidenavComponent,
+    DashboardComponent,
+    ProductsComponent,
+    StatisticsComponent,
+    CoupensComponent,
+    PagesComponent,
+    MediaComponent,
+    SettingsComponent
+  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forChild(routes)
+  ],
+  bootstrap:[MainAdminComponent,SidenavComponent]
 })
 export class AdminSideModule { }
